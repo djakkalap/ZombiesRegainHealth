d903d14e0d74eff4cc9bba3fffc1daa99482cc1a
djakkalap <thedoge2706@gmail.com>
2019-11-02 12:11:30 +0100

    Add project files.

---
+++ ZombiesRegainHealth/Events/ZombieKillEvent.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Smod2;
+using Smod2.API;
+using Smod2.Attributes;
+using Smod2.Config;
+using Smod2.EventHandlers;
+using Smod2.Events;
+using Smod2.Lang;
+using Smod2.Piping;
+
+namespace ZombiesRegainHealth.Events {
+    class ZombieKillEvent : IEventHandlerPlayerDie {
+        private readonly ZRHPlugin plugin;
+
+        public ZombieKillEvent(ZRHPlugin plugin) => this.plugin = plugin;
+
+        public void OnPlayerDie(PlayerDeathEvent ev) {
+
+            if (ev.Killer.TeamRole.Role == Role.SCP_049_2)
+            {
+
+            }
+        }
+    }
+}
---
+++ ZombiesRegainHealth/ZRHPlugin.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Smod2;
+using Smod2.API;
+using Smod2.Attributes;
+using Smod2.Config;
+using Smod2.EventHandlers;
+using Smod2.Events;
+using Smod2.Lang;
+using Smod2.Piping;
+
+using ZombiesRegainHealth.Events;
+
+namespace ZombiesRegainHealth {
+    [PluginDetails(
+        author = "djakkalap",
+        name = "ZombiesRegainHealth",
+        description = "This plugin heals SCP-049-2 instances when they kill someone.",
+        id = "djakkalap.zrhplugin",
+        configPrefix = "zrh",
+        version = "0.1",
+        SmodMajor = 3,
+        SmodMinor = 5,
+        SmodRevision = 0
+        )]
+
+    public class ZRHPlugin : Plugin {
+        public override void OnDisable() {
+            Info(Details.name + " has been disabled.");
+        }
+
+        public override void OnEnable() {
+            Info(Details.name + " has been enabled.");
+        }
+
+        // Register parts of the plugin.
+        public override void Register() {
+            AddEventHandler(typeof(IEventHandlerPlayerDie), new ZombieKillEvent(this));
+        }
+    }
+}
